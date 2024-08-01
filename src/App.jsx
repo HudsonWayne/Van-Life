10531f8e86b45ace86cafa969538ac0ea92578f2
VincentMugondora <vinmugondora@gmail.com>
2024-08-01 15:39:41 +0200

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import './App.css'
-import BarGraph from './Components/LogIn/Income'
-import SignInForm from './Components/LogIn/LogIn'
-import SignUpForm from './Components/LogIn/SignUp'
 
 function App() {
 
   return (
     <>
-      <SignInForm />
-      <SignUpForm />
-      <BarGraph />
+      
     </>
   )
 }
