90e109dc205c157f8cf6c49c9d65a31c2b94906e
Duke Chizengwe <dr.duke260@gmail.com>
2024-08-01 12:03:30 +0200

    Navbar and footer

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './style.css'
+import { FaRegCircleUser } from "react-icons/fa6";
+import Logo from '../assets/logog.png'
+
+
+
+const Navbar = () => {
+  return (
+    <div className='Navbar'>
+        <Link to='/'><img src={Logo} alt='Logo' className='logo' /></Link>
+       <nav>
+        <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/Host'>Host</Link></li>
+            <li><Link to='/Blog'>Vans</Link></li>
+            <li className='login-page'><Link to='/Login'><FaRegCircleUser /></Link></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
