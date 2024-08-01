39e1d130bf31e8262b574739e0c833a1b27e3668
Tapiwanashe Chakurungama <chakurungamatapiwanashe@gmail.com>
2024-08-01 11:41:39 +0200

    home

---
+++ src/assets/components/Home/Home.jsx
@@ -0,0 +1,24 @@
+import "./Home.css"
+function Home() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center text-white text-center">
+      <div className="container">
+        <h1 className="text-4xl font-bold mb-4">
+          You got the travel plans, we got the travel vans.
+        </h1>
+        <p className="text-lg mb-8">
+          Add adventure to your life by joining the #vanlife movement. Rent the
+          perfect van to make your perfect road trip.
+        </p>
+        <button
+          className="bg-orange-500   
+ hover:bg-orange-600 text-white font-bold py-2 px-4 rounded"
+        >
+          Find your van
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
