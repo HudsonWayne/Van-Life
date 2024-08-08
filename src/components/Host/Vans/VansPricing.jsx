f69f5a4c0f70113d3ad0ddc33f5e836c995d2e32
VincentMugondora <vinmugondora@gmail.com>
2024-08-08 11:38:23 +0200

    .

--- src/components/Host/Vans/VansPricing.jsx
+++ src/components/Host/Vans/VansPricing.jsx
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-
-=======
 import image from "../../../assets/Modest_Explorer rent.jpg";
 import "./VansPricing.css"
 import { Link } from "react-router-dom";
@@ -62,4 +59,3 @@ const VansPricing = () => {
 };
 
 export default VansPricing;
->>>>>>> 105c325653289f91cdb587446cdc572e4a1a261d
