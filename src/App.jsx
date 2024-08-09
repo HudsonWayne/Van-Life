ba4db0f7336d99815df93a93bea26460c96c87fb
AliciaManyeka <amanyeka8@gmail.com>
2024-08-09 13:59:19 +0200

    ccommit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import Login from "./components/Login";
 import About from "./Components/About.jsx"
 import Income from "./components/Host/Income.jsx"
 import VansHome from "./Components/Host/Vans/VansHome.jsx"
+import VansHome from "./Components/Host/Vans/VansDetails.jsx"
+import VansHome from "./Components/Host/Vans/VansPricing.jsx"
+import VansHome from "./Components/Host/Vans/VansPhotos.jsx"
 import Reviews from "./components/Host/Reviews.jsx";
 
 
@@ -25,6 +28,9 @@ const App = () => {
         <Route path="/Login" element={<Login />} />
         <Route path="/Host/Income" element={<Income/>}/>
         <Route path="/Host/Vans" element={<VansHome/>}/>
+        <Route path="/Host/Vans" element={<VansDetails/>}/>
+        <Route path="/Host/Vans" element={<VansPhotos/>}/>
+        <Route path="/Host/Vans" element={<VansPricing/>}/>
         <Route path="/Host/Reviews" element={<Reviews/>}/>
       </Routes>
       <Footer />
