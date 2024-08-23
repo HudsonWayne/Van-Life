11092e287ea0eee6a73522de8a4a1efda0bbe383
TinasheMuzenda <tmuzendat@gmail.com>
2024-08-23 10:14:45 +0200

    Merge with About-Intro

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,26 @@ import Navbar from "./components/Navbar";
 import Host from "./components/Host/Dashboard";
 import Login from "./components/Login";
 import About from "./components/About.jsx";
-import Income from "./components/Host/Income.jsx"
+import Income from "./components/Host/Income.jsx";
 import VansHome from "./components/Host/Vans/VansHome.jsx";
 import Reviews from "./components/Host/Reviews.jsx";
 
-
 const App = () => {
   return (
-
-<>
-<Navbar />
+    <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Host" element={<Host />} />
         <Route path="/About" element={<About />} />
         <Route path="/Vans" element={<Vans />} />
         <Route path="/Login" element={<Login />} />
-        <Route path="/Host/Income" element={<Income/>}/>
-        <Route path="/Host/Vans" element={<VansHome/>}/>
-        <Route path="/Host/Reviews" element={<Reviews/>}/>
+        <Route path="/Host/Income" element={<Income />} />
+        <Route path="/Host/Vans" element={<VansHome />} />
+        <Route path="/Host/Reviews" element={<Reviews />} />
       </Routes>
-      <Footer /></>
-  
+      <Footer />
+    </>
   );
 };
 
